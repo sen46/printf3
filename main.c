8756d5593eba443279b46e4ba5c6f6ed17fda534
sen46 <sw20050719@gmail.com>
2025-05-22 23:15:13 +0900

    modify ptr_bonus

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	int ret =       printf("=% #-+0p=\n",(void*)0x5d29fa520b20);
-	int ft_ret = ft_printf("=% #-+0p=\n",(void*)0x5d29fa520b20);
-	printf("ret = %d, ft_ret = %d\n", ret, ft_ret);
+	int ret =       printf("=% #-+02.3p=\n",(void*)0x5d29fa520b20);
+	// int ft_ret = ft_printf("=% #-+0p=\n",(void*)0x5d29fa520b20);
+	// printf("ret = %d, ft_ret = %d\n", ret, ft_ret);
 }
--- srcs/process_bonus/p_process/process_p_bonus.c
+++ srcs/process_bonus/p_process/process_p_bonus.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
 #include "../../../includes/ft_printf.h"
 
 static void	run(char *str, int len, t_flag *flag)
@@ -82,97 +81,25 @@ int	process_p_flag(t_flag *flag, va_list *ap)
 	{
 		flag->hash = 1;
 		pad.middle = ft_itoa_base((unsigned long)val, "0123456789abcdef");
+		flag->precision = ft_max(ft_strlen(pad.middle), flag->precision);
+		if (flag->hash)
+			flag->precision += 2;
+		if (flag->blank || flag->plus)
+			flag->precision++;
 	}
 	else
 	{
 		pad.middle = ft_strdup("(nil)");
+		if (flag->precision >= 6)
+		{
+			flag->width = ft_max(flag->width, flag->precision);
+			flag->precision = 5;
+		}
 		flag->zero = 0;
 	}
-	flag->precision = ft_max(ft_strlen(pad.middle), flag->precision);
-	if (flag->hash && val != 0)
-		flag->precision += 2;
-	if (flag->blank || flag->plus)
-		flag->precision++;
 	flag->width = ft_max(flag->width, flag->precision);
 	process_p_flag_sub(flag, &pad);
 	output_padding(flag, &pad, (unsigned long)val);
 	free_padding(&pad);
 	return (pad.llen + pad.mlen + pad.rlen);
 }
-*/
-
-
-#include "../../../includes/ft_printf.h"
-
-
-
-static void	add_prefix(char **str)
-{
-	char	*tmp = ft_strjoin("0x", *str);
-	free(*str);
-	*str = tmp;
-}
-
-static void	output_padding(t_flag *flag, t_padding *pad)
-{
-	if (flag->minus)
-	{
-		ft_putstr_fd(pad->middle, 1);
-		write(1, pad->right, pad->rlen);
-	}
-	else
-	{
-		write(1, pad->left, pad->llen);
-		ft_putstr_fd(pad->middle, 1);
-	}
-}
-
-static void	process_p_flag_sub(t_flag *flag, t_padding *pad)
-{
-	size_t	mid_len = ft_strlen(pad->middle);
-
-	if (flag->minus)
-	{
-		pad->right = ft_calloc(flag->width - mid_len + 1, 1);
-		ft_memset(pad->right, ' ', flag->width - mid_len);
-		pad->rlen = flag->width - mid_len;
-		pad->llen = 0;
-	}
-	else
-	{
-		char c = (flag->zero) ? '0' : ' ';
-		pad->left = ft_calloc(flag->width - mid_len + 1, 1);
-		ft_memset(pad->left, c, flag->width - mid_len);
-		pad->llen = flag->width - mid_len;
-		pad->rlen = 0;
-	}
-}
-
-int	process_p_flag(t_flag *flag, va_list *ap)
-{
-	void		*val;
-	t_padding	pad;
-
-	// 禁止フラグ強制オフ
-	flag->plus = 0;
-	flag->blank = 0;
-	flag->hash = 0;
-
-	val = va_arg(*ap, void *);
-	if (val)
-		pad.middle = ft_itoa_base((unsigned long)val, "0123456789abcdef");
-	else
-		pad.middle = ft_strdup("(nil)");
-
-	if (val)
-		add_prefix(&pad.middle);
-
-	flag->width = ft_max(flag->width, (int)ft_strlen(pad.middle));
-	process_p_flag_sub(flag, &pad);
-	output_padding(flag, &pad);
-
-	size_t ret = ft_strlen(pad.middle) + pad.llen + pad.rlen;
-	free_padding(&pad);
-	return (ret);
-}
-
